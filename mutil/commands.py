48fa7a74207de1c81fcc8a4cc8dde019a4ed5c24
Martin Kleiven <m.klei@live.no>
2017-08-06 11:38:02 +0200

    Write remove_playlist_duplicates feature

--- mutil/commands.py
+++ mutil/commands.py
@@ -5,3 +5,20 @@ def remove_playlist_duplicates(playlist):
 
     Returns: The number of duplicates removed.
     """
+    playlist_entries = []
+    duplicates = 0
+    for entry in playlist:
+        entry = entry.strip('\n')
+        if entry in playlist_entries:
+            duplicates += 1
+        else:
+            playlist_entries.append(entry)
+    new_playlist = '\n'.join(playlist_entries)
+
+    # Overwrite the old playlist
+    playlist.seek(0)
+    playlist.truncate()
+    playlist.write(new_playlist)
+    playlist.seek(0)
+
+    return duplicates
