aafc8e800ea77000ed1f48a6fc63d2262911d8c1
Martin Kleiven <m.klei@live.no>
2017-08-06 21:32:07 +0200

    Slightly revise docstring

--- mutil/commands.py
+++ mutil/commands.py
@@ -1,7 +1,7 @@
 def remove_playlist_duplicates(playlist):
     """Removes entries in a given playlist.
     Args:
-        playlist: A playlist to check.
+        playlist: A file containing a playlist to check.
 
     Returns: The number of duplicates removed.
     """
