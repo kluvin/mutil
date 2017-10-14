fb09b8f875121d9cabf35f75fa46fc0a508c2614
Martin Kleiven <m.klei@live.no>
2017-10-14 17:12:07 +0200

    I should test my code before I push

--- test/test_mutil.py
+++ test/test_mutil.py
@@ -3,7 +3,7 @@ import argparse
 from mutil import mutil
 
 
-def test_parser_initiates(self):
+def test_parser_initiates():
     parser = mutil.init_parser()
 
     assert isinstance(parser, argparse.ArgumentParser)
